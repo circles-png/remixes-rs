15f0a3ba52b6cf1d05d2cab54ce26fb49207be69
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-08-02 22:31:32 +1000

    move button remix and update main

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,20 @@
 #[warn(clippy::pedantic)]
 #[allow(clippy::cast_possible_truncation)]
 mod millis;
+mod remixes;
 
-use arduino_hal::{default_serial, pins, prelude::_void_ResultVoidExt, Peripherals};
-use avr_device::interrupt::enable;
-use millis::{init, millis};
-use ufmt::uwriteln;
+use arduino_hal::prelude::_void_ResultVoidExt;
+use core::panic::PanicInfo;
+use remixes::button_three_seconds;
 
 #[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-    // disable interrupts - firmware has panicked so no ISRs should continue running
+fn panic(info: &PanicInfo) -> ! {
     avr_device::interrupt::disable();
 
-    // get the peripherals so we can access serial and the LED.
-    //
-    // SAFETY: Because main() already has references to the peripherals this is an unsafe
-    // operation - but because no other code can run after the panic handler was called,
-    // we know it is okay.
     let dp = unsafe { arduino_hal::Peripherals::steal() };
     let pins = arduino_hal::pins!(dp);
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    // Print out panic location
     ufmt::uwriteln!(&mut serial, "Firmware panic!\r").void_unwrap();
     if let Some(loc) = info.location() {
         ufmt::uwriteln!(
@@ -38,65 +31,10 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
         .void_unwrap();
     }
 
-    // Blink LED rapidly
     let mut led = pins.d13.into_output();
     loop {
         led.toggle();
         arduino_hal::delay_ms(100);
-    }
-}
-
-fn button() -> ! {
-    let peripherals = Peripherals::take().unwrap();
-    let pins = pins!(peripherals);
-
-    let mut led = pins.d11.into_output();
-    let button = pins.d7.into_floating_input();
-
-    loop {
-        if button.is_high() {
-            led.set_high();
-        } else {
-            led.set_low();
-        }
-    }
-}
-
-fn button_reverse() -> ! {
-    let peripherals = Peripherals::take().unwrap();
-    let pins = pins!(peripherals);
-
-    let mut led = pins.d11.into_output();
-    let button = pins.d7.into_floating_input();
-
-    loop {
-        if button.is_low() {
-            led.set_high();
-        } else {
-            led.set_low();
-        }
-    }
-}
-
-fn button_three_seconds() -> ! {
-    let peripherals = Peripherals::take().unwrap();
-    let pins = pins!(peripherals);
-    let mut serial = default_serial!(peripherals, pins, 57600);
-
-    let mut led = pins.d11.into_output();
-    let button = pins.d7.into_floating_input();
-    init(&peripherals.TC0);
-    let mut last_off = millis();
-
-    loop {
-        if button.is_high() {
-            led.set_high();
-            last_off = millis();
-        } else if millis() - last_off > 3000 {
-            led.set_low();
-        }
-
-        uwriteln!(&mut serial, "millis: {}, last_off: {}", millis(), last_off).void_unwrap();
     }
 }
 
---
+++ src/remixes/button.rs
@@ -0,0 +1,53 @@
+use crate::millis::{init, millis};
+use arduino_hal::{pins, Peripherals};
+
+pub fn button() -> ! {
+    let peripherals = Peripherals::take().unwrap();
+    let pins = pins!(peripherals);
+
+    let mut led = pins.d11.into_output();
+    let button = pins.d7.into_floating_input();
+
+    loop {
+        if button.is_high() {
+            led.set_high();
+        } else {
+            led.set_low();
+        }
+    }
+}
+
+pub fn button_reverse() -> ! {
+    let peripherals = Peripherals::take().unwrap();
+    let pins = pins!(peripherals);
+
+    let mut led = pins.d11.into_output();
+    let button = pins.d7.into_floating_input();
+
+    loop {
+        if button.is_low() {
+            led.set_high();
+        } else {
+            led.set_low();
+        }
+    }
+}
+
+pub fn button_three_seconds() -> ! {
+    let peripherals = Peripherals::take().unwrap();
+    let pins = pins!(peripherals);
+
+    let mut led = pins.d11.into_output();
+    let button = pins.d7.into_floating_input();
+    init(&peripherals.TC0);
+    let mut last_off = millis();
+
+    loop {
+        if button.is_high() {
+            led.set_high();
+            last_off = millis();
+        } else if millis() - last_off > 3000 {
+            led.set_low();
+        }
+    }
+}
